9d3f75f252049acffce72893625e174b6dcaae23
chamod101 <chamoddperera99@gmail.com>
2024-03-24 11:29:51 +0530

    first commit

---
+++ models/productModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        Colour: {
+            type: String,
+            required: false,
+        }
+        ,
+        image: {
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
